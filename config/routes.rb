45afd47cbe9a896bba23606f2c849a62f6250c2a
monica andreoli <monicandreoli@gmail.com>
2021-02-18 14:55:57 +0100

    app working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: [:destroy]
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Cocktail.create(name: "Negroni")
+Cocktail.create(name: "Long Island Ice Tea")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Cosmopolitan")
+Cocktail.create(name: "Vodka Martini")
+Cocktail.create(name: "Pina Colada")
+Cocktail.create(name: "Gin Fizz")
+Cocktail.create(name: "Old Fashioned")
+
+
+
+Ingredient.destroy_all
+
+result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+
+drinks.each do |hash|
+  hash.each do |k, v|
+    Ingredient.create(name: v)
+  end
+end
+
+
