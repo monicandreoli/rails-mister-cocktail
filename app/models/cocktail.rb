d605b7ea42df1ee627a957331afec87a663a2318
monica andreoli <monicandreoli@gmail.com>
2021-02-19 11:17:36 +0100

    picture

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,13 @@ class Cocktail < ApplicationRecord
   has_many :reviews
 
   validates :name, presence: true, uniqueness: true, allow_blank: false
+
+  def cocktail_average_review
+    reviews_array = []
+    reviews.each { |review| reviews_array << review.rating }
+    if reviews.any?
+    reviews_array.sum / reviews_array.size
+    end
+  end
 end
+
